bb913110926157ab7b0945269c96b2e426b15ae7
ZHOU WU <zhouwu@zhous-mbp.home>
2026-01-20 23:24:05 -0500

    Add docker compose file

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 
 
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -29,22 +30,19 @@ dtype = {
 parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
 
-def run():
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_port = "5432"
-    pg_db = "ny_taxi"
-    year = 2021
-    month = 1
-
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 100000
-
+def run(
+    pg_user: str,
+    pg_pass: str,
+    pg_host: str,
+    pg_port: str,
+    pg_db: str,
+    year: int,
+    month: int,
+    target_table: str,
+    chunksize: int,
+):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
@@ -56,25 +54,48 @@ def run():
         chunksize=chunksize,
     )
 
-
     first = True
 
     for df_chunk in tqdm(df_iter):
-        if first: 
+        if first:
             df_chunk.head(n=0).to_sql(
-                name='target_table',   
-                con=engine, 
-                if_exists='replace'
+                name=target_table,
+                con=engine,
+                if_exists='replace',
             )
             first = False
-        
+
         df_chunk.to_sql(
-            name='target_table', 
-            con=engine, 
-            if_exists='append'
+            name=target_table,
+            con=engine,
+            if_exists='append',
         )
 
+@click.command()
+@click.option('--pg-user', default='root', show_default=True)
+@click.option('--pg-pass', default='root', show_default=True)
+@click.option('--pg-host', default='localhost', show_default=True)
+@click.option('--pg-port', default='5432', show_default=True)
+@click.option('--pg-db', default='ny_taxi', show_default=True)
+@click.option('--year', default=2021, type=int, show_default=True)
+@click.option('--month', default=1, type=int, show_default=True)
+@click.option('--target-table', default='yellow_taxi_data', show_default=True)
+@click.option('--chunksize', default=100000, type=int, show_default=True)
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    run(
+        pg_user=pg_user,
+        pg_pass=pg_pass,
+        pg_host=pg_host,
+        pg_port=pg_port,
+        pg_db=pg_db,
+        year=year,
+        month=month,
+        target_table=target_table,
+        chunksize=chunksize,
+    )
+
+
 if __name__ == "__main__":
-    run()
+    main()
 
 
